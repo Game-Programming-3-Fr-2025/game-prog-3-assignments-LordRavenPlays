eb7c78b4ed5f66ed5ca8edf2c2818e73c7a99044
LordRavenPlays <88121405+LordRavenPlays@users.noreply.github.com>
2025-09-05 12:42:39 -0400

    PickUp Command (?)
    The code doesn't really work but I'm hungry and going to get lunch now

--- Assets/Prototype 1/Scripts/PickUp.cs
+++ Assets/Prototype 1/Scripts/PickUp.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 
+//Uses code from https://www.youtube.com/watch?v=-V1O5FGQVY8&t=7s
+
 public class PickUp : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform holdSpot;
+    public LayerMask pickUpMask;
+
+    public Vector3 Direction { get; set; }
+    private GameObject itemHolding;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+            if (itemHolding)
+            {
+                itemHolding.transform.position = transform.position + Direction;
+                itemHolding.transform.parent = null;
+                if (itemHolding.GetComponent<Rigidbody2D>())
+                    itemHolding.GetComponent<Rigidbody2D>().simulated = true;
+                itemHolding = null;
+            }
+          
+            else
+            {
+                Collider2D pickUpItem = Physics2D.OverlapCircle(transform.position + Direction, .4f, pickUpMask);
+                if (pickUpItem)
+                {
+                    itemHolding = pickUpItem.gameObject;
+                    itemHolding.transform.position = holdSpot.position;
+                    itemHolding.transform.parent = transform;
+                    if (itemHolding.GetComponent<Rigidbody>())
+                        itemHolding.GetComponent<Rigidbody2D>().simulated = false;
+                }
+            }
+     
+                
+                
     }
 }

9b8c71efc235ed260475f59fdcc217b6245a6579
LordRavenPlays <88121405+LordRavenPlays@users.noreply.github.com>
2025-09-05 13:05:47 -0400

    Assets and stuff
    Try to just add enemies, picking up pots doesn't work for some reason
    
    Try looking through the video and the code, see if you missed anything.

--- Assets/Prototype 1/Scripts/PickUp.cs
+++ Assets/Prototype 1/Scripts/PickUp.cs
@@ -14,7 +14,7 @@ public class PickUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Space))
             if (itemHolding)
             {
                 itemHolding.transform.position = transform.position + Direction;
